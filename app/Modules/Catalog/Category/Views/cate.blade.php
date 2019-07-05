7bb0eeb2f01cfed1cc4c9ffd80ffa57d143a289b
Thuy Dung <duongthuydung91hn@gmail.com>
2019-07-05 15:56:48 +0700

    initial project version

--- app/Modules/Catalog/Category/Views/cate.blade.php
+++ app/Modules/Catalog/Category/Views/cate.blade.php
@@ -1,1 +1,10 @@
 <h1>Đây là trang view Category</h1>
+<div class="jumbotron">
+	<div class="container">
+		<h1>Hello, world!</h1>
+		<p>Contents ...</p>
+		<p>
+			<a class="btn btn-primary btn-lg">Learn more</a>
+		</p>
+	</div>
+</div>
